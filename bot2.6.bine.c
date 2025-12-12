f310d47000ff41b81aedf18f7a81103a21415740
Indrajith <indru2033bm@gmail.c0m>
2025-12-12 10:53:26 +0530

    new file

---
+++ bot2.6.bine.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int a[] = {10,20,30,40,50};
+    int n = 5;
+    int data, l = 0,r = n-1,mid;
+    printf("Enter element to search :");
+    scanf("%d",&data);
+    
+}
